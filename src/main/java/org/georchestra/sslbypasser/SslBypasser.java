9c3517e96e98273550dfdf9f4f18e054039aac9e
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-08-28 21:44:25 +0200

    Fixing some minor things
    - Adding logging via commons-logging
    - Cosmetic code changes

--- src/main/java/org/georchestra/sslbypasser/SslBypasser.java
+++ src/main/java/org/georchestra/sslbypasser/SslBypasser.java
@@ -1,15 +1,24 @@
 package org.georchestra.sslbypasser;
 
+import java.security.cert.X509Certificate;
+
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
 public class SslBypasser {
+    private static final Log LOG = LogFactory.getLog(SslBypasser.class.getName());
 
     static {
         try {
             SslBypasser.disableCertificatesCheck();
+            LOG.warn("SSL bypass in effect - Every SSL checks "
+                    + "from this context WILL BE DISCARDED !");
         } catch (Exception e) {}
     }
 
@@ -20,26 +29,22 @@ public class SslBypasser {
     public static void disableCertificatesCheck() throws Exception {
         TrustManager[] trustAllCerts = new TrustManager[]{
             new X509TrustManager() {
-
                 @Override
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                public X509Certificate[] getAcceptedIssuers() {
                     return null;
                 }
-
                 @Override
-                public void checkClientTrusted(
-                        java.security.cert.X509Certificate[] certs, String authType) {
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    return;
                 }
-
                 @Override
-                public void checkServerTrusted(
-                        java.security.cert.X509Certificate[] certs, String authType) {
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    return;
                 }
             }
         };
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 }
